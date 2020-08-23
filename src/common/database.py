04ee77c4fc9e59282c7a5f216e4f47d78f90ee89
dshen <dz507295@dal.ca>
2020-08-23 19:48:38 -0300

    database

--- src/common/database.py
+++ src/common/database.py
@@ -4,9 +4,9 @@ import pymongo
 
 
 class Database(object):
-    #URI = os.environ.get("MONGOLAB_URI")
+    URI = os.environ.get("MONGOLAB_URI")
     DATABASE = None
-    URI = "mongodb://127.0.0.1:27017"
+    #URI = "mongodb://127.0.0.1:27017"
 
 
     @staticmethod
