fa7734e92c63c70fd18b7de1c5ade8025f158ee0
dshen <dz507295@dal.ca>
2020-08-23 19:55:09 -0300

    database

--- src/common/database.py
+++ src/common/database.py
@@ -12,7 +12,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = client['blog']
         #Database.DATABASE = client.get_default_database()   Database.DATABASE = client['fullstack']
 
 
