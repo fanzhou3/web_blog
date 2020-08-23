04e56d68373fcec469f669956a6b80be432482f9
dshen <dz507295@dal.ca>
2020-08-23 19:45:56 -0300

    database

--- src/common/database.py
+++ src/common/database.py
@@ -4,17 +4,19 @@ import pymongo
 
 
 class Database(object):
-    URI = os.environ.get("MONGOLAB_URI")
+    #URI = os.environ.get("MONGOLAB_URI")
     DATABASE = None
-    #URI = "mongodb://127.0.0.1:27017"
+    URI = "mongodb://127.0.0.1:27017"
 
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = client['fullstack']
+        #Database.DATABASE = client.get_default_database()
 
-   #Database.DATABASE = client['fullstack']
+
+
 
     @staticmethod
     def insert(collection, data):
