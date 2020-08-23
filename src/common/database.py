32aca985e5d76c675b674900ed9088e6a3f3258f
dshen <dz507295@dal.ca>
2020-08-23 20:24:55 -0300

    database

--- src/common/database.py
+++ src/common/database.py
@@ -6,14 +6,14 @@ import pymongo
 class Database(object):
     #URI = os.environ.get("MONGOLAB_URI")
     DATABASE = None
-    URI="mongodb://<fanzhou>:<fanzhou3>@ds041561.mlab.com:41561/heroku_g3q7pzwj"
+    URI="mongodb://fanzhou:fanzhou3@ds041561.mlab.com:41561/heroku_g3q7pzwj"
     #URI = "mongodb://127.0.0.1:27017"
 
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['heroku_g3q7pzwj']
+        Database.DATABASE = client.get_default_database()
         #Database.DATABASE = client.get_default_database()    client['fullstack']  client['heroku_g3q7pzwj']
 
 
